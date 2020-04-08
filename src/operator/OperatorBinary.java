0882db537a37a8f752a8187a9efc2f3f9a77553d
mirzayogy <mirza.yogy@gmail.com>
2020-04-08 15:14:25 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,14 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assignment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Mirza Yogy Kurniawan";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+
+    
+}
